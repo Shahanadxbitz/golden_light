41b5954d49642e06a605fdb08bb015cf21f33326
rayen <rayenlabdouni@gmail.com>
2023-07-31 13:53:09 +0400

    remove

--- client_golden_light/public/js/client_scripts/forms/payment_entry.js
+++ client_golden_light/public/js/client_scripts/forms/payment_entry.js
@@ -1,5 +1,14 @@
 frappe.ui.form.on('Payment Entry', {
 
+	onload(frm) {
+
+		// show currency conversion rate when non-default currency
+		if(frm.doc.paid_to_account_currency != frm.doc.paid_from_account_currency) {
+		    frm.set_df_property('paid_amount', 'read_only', 1);
+		    if(frm.doc.target_exchange_rate > 0)
+		        frm.set_value('received_amount', frm.doc.paid_amount / frm.doc.target_exchange_rate);
+		}
+	},
     refresh(frm) {
         frm.set_query('party_type', function() {
             return {
@@ -18,15 +27,7 @@ frappe.ui.form.on('Payment Entry', {
 		}
 	},
 
-	paid_to_account_currency(frm) {
 
-		// show currency conversion rate when non-default currency
-		if(frm.doc.paid_to_account_currency != frm.doc.paid_from_account_currency) {
-		    frm.set_df_property('paid_amount', 'read_only', 1);
-		    if(frm.doc.target_exchange_rate > 0)
-		        frm.set_value('received_amount', frm.doc.paid_amount / frm.doc.target_exchange_rate);
-		}
-	},
 	received_amount(frm) {
 	    frm.set_value('paid_amount', frm.doc.received_amount * frm.doc.target_exchange_rate);
 	},
