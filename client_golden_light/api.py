a4af91483d5d2243130f7b7a6d603bc3b8e513ce
rayen <rayenlabdouni@gmail.com>
2024-05-25 15:04:20 +0400

    whitelist

--- client_golden_light/api.py
+++ client_golden_light/api.py
@@ -1,9 +1,11 @@
 import frappe
 
+@frappe.whitelist()
 def get_permitted_divisions(user):
     divisions = frappe.db.get_all("Division User", { "filters": { "parenttype": 'Division', "user": user }, "fields": ['parent'], "pluck": 'parent'})
     return divisions
 
+@frappe.whitelist()
 def get_permitted_warehouses(divisions):
     warehouses =  frappe.db.get_all("Division Permission", {
         "filters": { "parenttype": 'Division', "document_type": 'Warehouse', "parent": ['in', divisions], "applicable_for": ['in', ["", "Stock Entry"]] },
