5c1b7cf8d431d5360be79bdc0375db4409266aa1
aswincalicut <aswincalicut57@gmail.com>
2024-12-24 16:10:00 +0530

    fix:Restrict voucher_type editing to System Manager and Entry Type roles

--- client_golden_light/public/js/client_scripts/forms/journal_entry.js
+++ client_golden_light/public/js/client_scripts/forms/journal_entry.js
@@ -1,8 +1,12 @@
 frappe.ui.form.on('Journal Entry', {
     setup(frm) {
-		frm.set_df_property('voucher_type',  'read_only',  frappe.user.has_role("System Manager") ? 0 : 1);
-	},
-	refresh(frm) {
-		frm.set_df_property('voucher_type',  'read_only',  frappe.user.has_role("System Manager") ? 0 : 1);
-	}
-})
+        frm.set_df_property('voucher_type', 'read_only', has_permission_to_edit() ? 0 : 1);
+    },
+    refresh(frm) {
+        frm.set_df_property('voucher_type', 'read_only', has_permission_to_edit() ? 0 : 1);
+    }
+});
+
+function has_permission_to_edit() {
+    return frappe.user.has_role('System Manager') || frappe.user.has_role('Entry Type');
+}
